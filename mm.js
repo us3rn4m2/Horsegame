053161fb374c84aeaca1c7990ec8b1f7e19ce586
us3rn4m2 <120786101+us3rn4m2@users.noreply.github.com>
2022-12-21 23:44:09 +0900

    Delete mm.js

--- mm.js
+++
@@ -1,3 +0,0 @@
-const myRequest = new Request('https://www.thelog.co.kr/api/common/getCommonState.do', {method: 'GET',body: '{"gameDataType": "S"}'});
-
-const myRequest = new Request('https://www.thelog.co.kr/api/common/getCommonState.do', {method: 'POST', body: '{"gameDataType":"S"}'});
